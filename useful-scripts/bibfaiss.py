8d62eb470f303bab043fd06cd2aff6848d0cd6fb
Julio Cesar <jcmcs177@gmail.com>
2025-02-12 17:21:02 -0300

    remove trust code

--- useful-scripts/bibfaiss.py
+++ useful-scripts/bibfaiss.py
@@ -3,6 +3,6 @@ from aisurveywriter.core.text_embedding import load_embeddings
 from aisurveywriter.core.latex_handler import write_latex
 
 model = "Salesforce/SFR-Embedding-Mistral"
-embed = load_embeddings(model, "huggingface", trust_remote_code=True)
+embed = load_embeddings(model, "huggingface")
 ref = PaperFAISSReferencer(embed, "../bib/refextract-21papers.bib", local_faissdb_path=None, save_usedbib_path="temp/test.bib", 
                            save_faiss_path=f"temp/{model[model.find('/')+1:]}", max_per_section=60, max_per_sentence=1,confidence=0.7)
