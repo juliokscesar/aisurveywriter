ecf9daa126aaf62f26f388b7c3c0823d48ebeab7
Julio Cesar <jcmcs177@gmail.com>
2025-02-12 18:23:43 -0300

    filtered img faiss for some small models

--- useful-scripts/bibfaiss.py
+++ useful-scripts/bibfaiss.py
@@ -2,7 +2,8 @@ from aisurveywriter.tasks.paper_faiss_ref import PaperFAISSReferencer, PaperData
 from aisurveywriter.core.text_embedding import load_embeddings
 from aisurveywriter.core.latex_handler import write_latex
 
-model = "Salesforce/SFR-Embedding-Mistral"
+path = "/home/juliocesar/dev/aisurveywriter"
+model = "BAAI/bge-m3"
 embed = load_embeddings(model, "huggingface")
 ref = PaperFAISSReferencer(embed, "../bib/refextract-21papers.bib", local_faissdb_path=None, save_usedbib_path="temp/test.bib", 
-                           save_faiss_path=f"temp/{model[model.find('/')+1:]}", max_per_section=60, max_per_sentence=1,confidence=0.7)
+                           save_faiss_path=f"{path}/bib/{model[model.find('/')+1:]}-bibfaiss", max_per_section=60, max_per_sentence=1,confidence=0.7)
--- useful-scripts/figfaiss.py
+++ useful-scripts/figfaiss.py
@@ -6,19 +6,19 @@ from aisurveywriter.core.file_handler import read_credentials, read_yaml
 from aisurveywriter.core.text_embedding import load_embeddings
 from aisurveywriter.utils import get_all_files_from_paths
 
-path = "/home/juliocesar/aisurveywriter"
+path = "/home/juliocesar/dev/aisurveywriter"
 os.environ["GOOGLE_API_KEY"] = read_credentials(f"{path}/credentials.yaml")["google_key"]
 
-img_data = read_yaml(f"{path}/bib/refextract-imgdata.yaml")["data"]
-img_data = [img for img in img_data if "OliveiraO2022" not in img["path"]]
+img_data = read_yaml(f"{path}/bib/filtered-imgdata.yaml")["data"]
 
 llm = LLMHandler(model="gemini-2.0-flash", model_type="google")
-embed = load_embeddings("Alibaba-NLP/gte-Qwen2-1.5B-instruct", "huggingface")
+model = "BAAI/bge-m3"
+embed = load_embeddings(model, "huggingface")
 fig = FigureExtractor(
     llm, embed, 
     "Langmuir and Langmuir-Blodgett films", 
     pdf_paths=get_all_files_from_paths(f"{path}/refexamples", stem_sort=True), 
-    save_dir=f"./save", faiss_save_path=f"{path}/bib/qwen2-1.5B-nochuimgfaiss", 
-    local_faiss_path=None, paper=None, request_cooldown_sec=15,
+    save_dir=f"./save", faiss_save_path=f"{path}/bib/{model[model.rfind('/')+1:]}-allimgfaiss", 
+    local_faiss_path=None, paper=None, request_cooldown_sec=0,
 )
 faiss = fig.create_faiss(img_data)
